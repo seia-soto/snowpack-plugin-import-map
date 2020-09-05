5f4c353aeeb16611e660e4bcece8942481a7bf8b
Paosder <denjaraos@gmail.com>
2020-09-05 11:39:44 +0900

    fix issues & add options

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ const plugin = require('./babel-plugin.js');
 
 // Snowpack Plugin
 module.exports = function (snowpackConfig, pluginOptions) {
+  
+  const extensions = pluginOptions.extensions || ['.js', '.jsx','.tsx', '.ts']; // extensions.
+  const dev = pluginOptions.dev || false; // for debugging improvements in development.
+  const imports = pluginOptions.imports || {};
 
   return {
+    async transform(options) {
+      
+      const { contents, filePath, isDev, fileExt } = options;
 
-    // build source with babel
-    async build(options) {
-      const { contents, filePath, isDev } = options;
-
-      const result = await transformAsync(contents, {
-        filename: filePath,
-        plugins: [plugin(pluginOptions)],
-        cwd: process.cwd(),
-        ast: false,
-      });
-
-      let code = result.code;
-
-      return {
-        result: code,
+      if (!(isDev === true && dev === true) &&
+        extensions.includes(fileExt.toLowerCase())) {
+        const result = await transformAsync(contents, {
+          filename: filePath,
+          plugins: [plugin(imports)],
+          cwd: process.cwd(),
+          ast: false,
+        });
+        return result.code;
       }
+      return contents;
     },
   };
 };
