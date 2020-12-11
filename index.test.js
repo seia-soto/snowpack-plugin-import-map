7eba2e601019d07b667998db9e7c30d6a576b532
Seia-Soto <seia@outlook.kr>
2020-12-11 11:41:37 +0900

    Add missed cases

--- index.test.js
+++ index.test.js
@@ -142,6 +142,9 @@ test("regex", () => {
     import * as name from "module-name";
     import { export1 } from "module-name";
     import { export1 as alias1 } from "module-name";
+    import { export1, export2 } from "module-name";
+    import { export1, export2 as alias2 } from "module-name";
+    import defaultExport, { export1, export2 } from "module-name";
     import defaultExport, * as name from "module-name";
     import "module-name";
     var promise = import("module-name");
