58f8194a68f7d0ebd626b71077e9506121144e49
Seia-Soto <seia@outlook.kr>
2020-12-11 11:34:30 +0900

    Fix regex for new case and modified the test

--- esmImportRegex.js
+++ esmImportRegex.js
@@ -1,4 +1,4 @@
 // Matches any ES6 import and returns 3 groups: everything up to the module name, the module name, and anything after.
 // It only matches package imports, not relative or absolute imports.
-const esmImportRegex = /(import(?:\s+[\w*${}\s,]+from\s*)?\(?\s*['"])((?:@[\w-]+\/)?[\w-]+)(['"](?:\s*\))?)/gm;
+const esmImportRegex = /(import(?:\s+[\w*${}\s,]+from\s*)?\(?\s*['"])((?:@[\w-]+\/)?[\w-\/]+)(['"](?:\s*\))?)/gm;
 module.exports = esmImportRegex;
